7d95d7ae77b6d409f4698a92cc3f1d7fef3f8054
Max Nigmatulin <mvnigma@gmail.com>
2023-08-17 18:17:54 +0300

    Working export

--- VSharp.ML.AIAgent/onnx_test.py
+++ VSharp.ML.AIAgent/onnx_test.py
@@ -1,25 +1,51 @@
+import onnx
+import onnxruntime
 import torch
-
+import numpy as np
 from config import GeneralConfig
-from ml.onnx.onnx_import import export_onnx_model
-from ml.onnx.onnx_import import create_torch_dummy_input, create_onnx_dummy_input
+from ml.onnx.onnx_import import (
+    create_onnx_dummy_input,
+    create_torch_dummy_input,
+    export_onnx_model,
+)
+from new_encoder import StateModelEncoder
+
+DUMMY_PATH = "ml/onnx/dummy_input.json"
 
 
 def main():
-    model = GeneralConfig.EXPORT_MODEL_INIT()
-    model.load_state_dict(torch.load("test_model.pth"))
-    model.eval()
+    model = StateModelEncoder(32, 8)
 
-    model = GeneralConfig.EXPORT_MODEL_INIT()
-    model.load_state_dict(torch.load("test_model.pth"))
-    a, b, c = create_torch_dummy_input()
-    traced_cell = torch.jit.trace(model, (a, b, c), strict=False)
-    print(traced_cell)
-    print(traced_cell.graph)
-    print(traced_cell.code)
-    traced_cell(a, b, c)
+    x = model.convert_to_single_tensor(*create_torch_dummy_input(), False)
 
-    export_onnx_model(model, save_path="test_model.onnx")
+    torch_out = model.forward(x)
+    # traced_cell = torch.jit.trace(model, x, strict=False)
+    # print(traced_cell)
+    # print(traced_cell.graph)
+    # print(traced_cell.code)
+    # traced_cell(x)
+
+    save_path = "test_model.onnx"
+
+    torch.onnx.export(
+        model=model,
+        args=x,
+        f=save_path,
+        verbose=False,
+        do_constant_folding=True,
+        input_names=["x"],
+    )
+
+    model = onnx.load(save_path)
+    onnx.checker.check_model(model)
+    # print(onnx.helper.printable_graph(model.graph))
+
+    ort_in = x.numpy()
+    ort_session = onnxruntime.InferenceSession(save_path)
+    ort_outs = ort_session.run(None, {"x": ort_in})
+
+    print(torch_out)
+    print(ort_outs)
 
 
 if __name__ == "__main__":
